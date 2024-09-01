193b81ee73b1749a1feee105d63257fb4759c904
MehwishI <mehwishned21@gmail.com>
2024-09-01 17:27:17 -0500

    Todo API

---
+++ server.js
@@ -0,0 +1,106 @@
+
+const express = require('express');
+const morgan = require('morgan');
+const {uid} = require('uid');
+
+const PORT = 8080;
+//{"task": "Buy groceries", "completed": false}
+const todoList = [{
+  id: '1',
+  task: "Buy groceries",
+  completed: false
+},
+{
+  id: '2',
+  task: "Doing Laundry",
+  completed: true
+}];
+const app = express();
+app.set('view engine', 'ejs');
+app.use(morgan('dev'));
+app.use(express.urlencoded({extended : true}));
+
+app.get('/', (req, res) => {
+    res.render('index');
+});
+
+app.get('/api/todos', (req, res) => {
+    const templateVars = {
+        todoList: todoList
+        statusCode: 200;
+    };
+    res.render('todos', templateVars);
+});
+
+app.get('/api/todo/form', (req, res) => {
+    res.render('new-todo');
+});
+
+app.get('/api/todos/:id', (req, res) => {
+    const todoId = req.params.id;
+    const foundTodo = todoList.find((todo) => {
+        if(todo.id === id){
+            return todoId;
+        }
+    });
+    const templateVars = {
+        todo : foundTodo,
+        issue: false,
+        statusCode: null
+    }
+    if(!foundTodo){
+        templateVars.issue = "This task doesn't exists!";
+        templateVars.statusCode = 500;
+    }
+    console.log(templateVars );
+    res.render('todo-detail', templateVars);
+});
+
+app.get('/api/todo/edit/:Id', (req, res) => {
+    const todoId = req.params.Id;
+    const foundTodo = todoList.find((todo) => {
+        if(todo.id === todoId){
+            return todoId;
+        }
+    });
+    const templateVars = {
+        todo : foundTodo
+    }
+    res.render('edit-todo', templateVars);
+});
+
+app.post('/api/todo/new', (req, res) => {
+    const newTodo = req.body;
+    newTodo.id = uid(3);
+  
+    todoList.push(newTodo);
+    res.redirect('/api/todos');
+});
+
+app.post('/api/edit/todo/:id', (req, res) => {
+    const todoId = req.params.id;
+    for(let i = 0; i < todoList.length; i ++){
+        if(todoList[i].id === id){
+            todoList[i].task = req.body.task;
+            todoList[i].completed = req.body.completed;
+        }
+    }
+    res.redirect(`/api/todos/${todoId}`);
+});
+
+app.post('/api/delete/todo/:id', (req, res) => {
+    const todoId = req.params.id;
+    const todoIndex = todoList.findIndex((todo) => {
+        if(todoId === todo.id){
+            return todo;
+        }
+    });
+   todoList.splice(todoIndex, 1);
+    res.redirect('/api/todos');
+});
+
+app.listen(PORT, () => {
+    console.log(`This server is running in port ${PORT}.`);
+});
+
+// http://localhost:8080/
